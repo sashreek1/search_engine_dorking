9a37ad2667d4c3a379da2effd452edf5cace3944
sashreek1 <45600974+sashreek1@users.noreply.github.com>
2019-12-09 22:07:52 +0530

    added bing support

--- dorking.py
+++ dorking.py
@@ -1,28 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml.html
-escaped_search_term = input('Search term must be a string : ')
-number_results = int(input("number of required results : "))
-def fetch_results(escaped_search_term,number_results):
+def fetch_results(escaped_search_term):
     language_code = "en"
-    google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results,language_code)
+    google_url = 'https://www.google.com/search?q={}&hl={}'.format(escaped_search_term,language_code)
     response = requests.get(google_url)
     return response
+def fetch_results_bing(term):
+    bing_url = 'https://www.bing.com/search?q='+term
+    response = requests.get(bing_url)
+    return response, bing_url
 
 
 if __name__ == '__main__':
-    html = fetch_results(escaped_search_term,number_results)
-    doc = lxml.html.fromstring(html.content)
-    results = doc.xpath('//div[@class="BNeawe UPmit AP7Wnd"]/text()')
-    print()
-    print("#####################################################################")
-    print("searching for :", escaped_search_term)
-    print("#####################################################################")
-    print()
-    for i in results:
-        print(i)
-    print()
-    print()
-    print("#####################################################################")
-    print("returned", len(results),"results")
-    print("#####################################################################")
+    se = int(input("please enter favourable search engine : \n 1. Google\n 2. Bing \n"))
+    escaped_search_term = input('Search term must be a string : ')
+    if se == 1:
+        html = fetch_results(escaped_search_term)
+        doc = lxml.html.fromstring(html.content)
+        results = doc.xpath('//div[@class="BNeawe UPmit AP7Wnd"]/text()')
+        print()
+        print("#####################################################################")
+        print("searching for :", escaped_search_term)
+        print("#####################################################################")
+        print()
+        for i in results:
+            print(i)
+        print()
+        print()
+        print("#####################################################################")
+        print("returned", len(results),"results")
+        print("#####################################################################")
+    else:
+        html, url = fetch_results_bing(escaped_search_term)
+        doc = lxml.html.fromstring(html.content)
+        results = doc.xpath('//li[@class="b_algo"]/h2/a[1]/@href')
+        print()
+        print("#####################################################################")
+        print("searching for :", escaped_search_term, "at :", url)
+        print("#####################################################################")
+        print()
+        for i in results:
+            print(i, "\n")
+        print()
+        print()
+        print("#####################################################################")
+        print("returned", len(results), "results")
+        print("#####################################################################")
